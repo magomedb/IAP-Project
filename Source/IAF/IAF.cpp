9ab5feab9fd3bb98273ce91edaa2cb56737e4523
Maindz <mariusfridheim185@gmail.com>
2020-02-07 12:55:32 +0100

    CPPComponent for Actor(last failed)

---
+++ Source/IAF/IAF.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IAF.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IAF, "IAF" );
---
+++ Source/IAF/RL.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IAF_API URL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
